acc8ef1475331154ae0dc0812058428fa57c628b
Mostafa Ibrahim <11410120200534@stud.cu.edu.eg>
2024-10-12 08:58:05 +0300

    Fix some bugs with links

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d1dbf2763ecb55adc232d1e7a5da00bf6046b23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdca01f9300f611a129fa1b387b2c96bd5007acd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
